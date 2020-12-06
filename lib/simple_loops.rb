e6859daef3fbfb07fbd7a56ded7a81b6a6975b99
William Bugenis <wbugenis@gmail.com>
2020-12-06 04:36:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,11 +12,7 @@ def loop_message_n_times(msg, n)
 end
 
 def output_array(array){
-  counter = 0
-  while(counter < array.length) do
-    puts array[counter]
-    counter += 1 
-  end
+  
 end
 
 def return_string_array(array)
